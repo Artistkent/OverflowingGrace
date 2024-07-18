58452164efcc496903e6efd0cd4fc5f8bbcaf6f1
Artistkent <collins.ose5@gmail.com>
2024-07-18 13:34:33 +0100

    commit
    commit

---
+++ src/app/components/carrier.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+
+
+const Carrier = ({imgsrc, alttext}:{ imgsrc: string, alttext:string}) => {
+  return (
+    <>
+    <a
+          
+          className="group rounded-lg border my-5 mx-2 transition-colors border-gray-300 bg-white dark:border-neutral-700 dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+            <div className="items-center rounded-lg grid md:grid-cols-2 text-left lg:mb-0 lg:w-full lg:max-w-5xl overflow-hidden">
+
+
+            <Image draggable="false"
+            className="h-full sm:w-full "
+          src={imgsrc}
+          width={500}
+          height={500}
+          alt={alttext}
+            />
+            
+
+          <div id="text-container" className="group h-full p-4 grid  transition-colors border-gray-300 dark:border-neutral-700 dark:bg-neutral-800/30">
+
+
+      <span className="group h-fit w-fit py-1 px-4 rounded-2xl transition-colors border-gray-300 bg-yellow-300 0">
+      Top Property
+      </span>
+
+            <h3 className="font-semibold m-0 ">
+            &#8358; 700,000
+            </h3>
+
+          <p className="m-0 text-sm ">
+            Awesome land! There's so much you can do here! Plant flowers, plant seeds, plant virus flowers, plant elemental evolution flowers, plant nuclear flowers, plant hyfroxinated flowers...
+          </p>
+
+          </div>
+
+          </div>
+
+          
+        </a>
+    </>
+  );
+};
+
+export default Carrier;
---
+++ src/app/components/navigation/navbar.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Link from "next/link";
+
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="w-full h-20 sticky top-0">
+        <div className="container mx-auto px-4 h-full">
+          <div className="flex justify-around items-center h-full">
+            {/* <Logo /> */}
+            <ul className="hidden md:flex gap-x-6 ">
+            <li>
+                <Link href="/buy">
+                  <p>Buy</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/sell">
+                  <p>Sell</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/about">
+                  <p>About</p>
+                </Link>
+              </li>
+              
+              <li>
+                <Link href="/contacts">
+                  <p>Contacts</p>
+                </Link>
+              </li>
+
+            </ul>
+            {/* <Button /> */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
